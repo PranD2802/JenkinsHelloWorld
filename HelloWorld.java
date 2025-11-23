eace8a912d352282033b6bf048c2f1fb17b58a8b
PranD2802 <83543273+PranD2802@users.noreply.github.com>
2025-11-23 21:09:53 +0530

    Made some changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld{
 	public static void main(String args []){
 		System.out.println("Hello world");
-		System.out.println("Welcome");
 		System.out.println("And we are adding Poll SCM and Webhooks");
 	}
 }
