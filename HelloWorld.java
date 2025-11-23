92174cbac2888aa9d3429db1aa18baee25257987
PranD2802 <83543273+PranD2802@users.noreply.github.com>
2025-11-23 20:55:12 +0530

    Made some changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,6 @@ public class HelloWorld{
 	public static void main(String args []){
 		System.out.println("Hello world");
 		System.out.println("Welcome");
-		System.out.println("This is our first Jenkins project");
 		System.out.println("And we are adding Poll SCM and Webhooks");
 	}
 }
