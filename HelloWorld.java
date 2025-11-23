760dee1bb5d1d8dac9324f598cb87a71d437b624
PranD2802 <83543273+PranD2802@users.noreply.github.com>
2025-11-23 21:03:16 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,5 @@ public class HelloWorld{
 		System.out.println("Hello world");
 		System.out.println("Welcome");
 		System.out.println("And we are adding Poll SCM and Webhooks");
-		System.out.println("Trying again after changes");
 	}
 }
