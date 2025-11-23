2fb80131b1df1366baa24aa45c723474fd093938
PranD2802 <83543273+PranD2802@users.noreply.github.com>
2025-11-23 20:46:29 +0530

    Made changes for Webhooks

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld{
 		System.out.println("Hello world");
 		System.out.println("Welcome");
 		System.out.println("This is our first Jenkins project");
+		System.out.println("And we are adding Poll SCM and Webhooks");
 	}
 }
