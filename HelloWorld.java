46d0e99af75db0ad1368ded94ab640151f4c37c7
PranD2802 <83543273+PranD2802@users.noreply.github.com>
2025-11-23 20:21:28 +0530

    Made some changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld{
 	public static void main(String args []){
 		System.out.println("Hello world");
+		System.out.println("Welcome");
 		System.out.println("This is our first Jenkins project");
 	}
 }
